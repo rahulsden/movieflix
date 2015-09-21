7277fcd0b0012e3aa9e41f5a28186cec914b0a94
facebook-overlap <rahulgupta999@gmail.com>
2015-09-21 20:40:14 +0530

    updated after making the code review comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,32 +1,26 @@
-#import statements
+# import statements
 from media import Movie
 from fresh_tomatoes import *
 
-#function to add the favourite movies in the list
-#create object of class 'Movie' and append in the input list argument 'movie_list'
+# function to add the favourite movies in the list
+# create object of class 'Movie' and append in the input list 'movie_list'
+
 def add_fav_movies(movie_list):
 	movie_list.append(Movie("The Shawshank Redemption",
-					  "http://ia.media-imdb.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1__SX727_SY685_.jpg",
-					  "https://www.youtube.com/watch?v=6hB3S9bIaco"
-					  ))
+					"http://ia.media-imdb.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1__SX727_SY685_.jpg",		# noqa
+					"https://www.youtube.com/watch?v=6hB3S9bIaco"))
 
 	movie_list.append(Movie("The Godfather",
-					  "http://ia.media-imdb.com/images/M/MV5BMjEyMjcyNDI4MF5BMl5BanBnXkFtZTcwMDA5Mzg3OA@@._V1__SX727_SY685_.jpg",
-					  "https://www.youtube.com/watch?v=sY1S34973zA"
-					  ))
-
+					"http://ia.media-imdb.com/images/M/MV5BMjEyMjcyNDI4MF5BMl5BanBnXkFtZTcwMDA5Mzg3OA@@._V1__SX727_SY685_.jpg",		# noqa
+					"https://www.youtube.com/watch?v=sY1S34973zA"))
 
 	movie_list.append(Movie("The Dark Knight",
-					  "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1__SX727_SY685_.jpg",
-					  "https://www.youtube.com/watch?v=EXeTwQWrcwY"
-					  ))
+					"http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1__SX727_SY685_.jpg",		# noqa
+					"https://www.youtube.com/watch?v=EXeTwQWrcwY"))
+	# add more movies here
 
-	#add more movies here
-
-
-
-#create an empty favourite movie list
-my_fav_movie_list = [];
+# create an empty favourite movie list
+my_fav_movie_list = []
 
 # call function to add the movies in the list
 add_fav_movies(my_fav_movie_list)
--- media.py
+++ media.py
@@ -1,12 +1,12 @@
-#This class defines the data structure for the Movie object
+# This class defines the data structure for the Movie object
 class Movie:
 
-	#constructor with 3 arguments
+	# constructor with 3 arguments
 	# 1) Movie Title
 	# 2) Complete url for movie poster
 	# 3) Complete youtube url for movie trailer
 	def __init__(self, title, poster_url, trailer_url):
-		self.title = title;
-		self.poster_image_url = poster_url;
-		self.trailer_youtube_url = trailer_url;
-		self.imdb_rating = -1;  #to be used for future enhancement
+		self.title = title
+		self.poster_image_url = poster_url
+		self.trailer_youtube_url = trailer_url
+		self.imdb_rating = -1		# to be used for future enhancement
